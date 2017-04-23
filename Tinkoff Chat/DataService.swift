bc43dfaf4e1c0312fc59dd79850ed57d850ebeca
andrey <andreynagaev@mail.ru>
2017-04-23 21:11:11 +0300

    Last SOA Commit

---
+++ Tinkoff Chat/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  Tinkoff Chat
+//
+//  Created by Андрей on 22.04.17.
+//  Copyright © 2017 Andrey. All rights reserved.
+//
+
+import Foundation
+
+protocol DataService {
+    func saveData(_ dictionary: Dictionary<String, Any>?)
+    func readData()
+    
+    var delegate: DataServiceDelegate?{ get set}
+}
